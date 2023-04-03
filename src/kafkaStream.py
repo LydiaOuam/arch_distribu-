e28fa9c4089e75720040600288bb5164ece90d15
LydiaOuam <84903904+LydiaOuam@users.noreply.github.com>
2023-04-03 22:55:02 +0200

    update stream File

--- src/kafkaStream.py
+++ src/kafkaStream.py
@@ -1,7 +1,23 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, LongType, TimestampType
+import time
+from pyspark.sql import SparkSession
+spark = SparkSession.builder\
+        .appName("SparkML") \
+        .config("spark.some.config.option", "some-value")\
+        .master("spark://0622870c1427:7077") \
+        .config("spark.executor.memory", "3g") \
+        .getOrCreate()
 
-WRITE_PATH = "/data"
+
+# df2 = spark.read.option("header","false").option("delimiter",';').csv("/../../../src/testdata")
+df = spark.read.option("header","true").option("delimiter",';').csv("/../../../src/testdata")
+df = df.collect()
+for i in range(len(df)):
+    message = df[i]  # Code pour récupérer le prochain message à envoyer
+    print(message)  # Code pour envoyer le message
+    time.sleep(0.1)  # Délai de 100 millisecondes entre chaque message
+
 
 spark = SparkSession.builder.appName('transformParquet').getOrCreate()
 
@@ -18,6 +34,7 @@ SCHEMA = StructType([
     StructField("SENTIMENT", LongType())
 ])
 
+print("hello")
 
 
 
@@ -25,10 +42,9 @@ SCHEMA = StructType([
 
 
 
-
-spark.read\
-    .option("multiline", "true")\
-    .json(files, schema=SCHEMA)\
-    .write\
-    .mode('overwrite')\
-    .parquet(join(WRITE_PATH, folder.split('/')[-1]))
+# spark.read\
+#     .option("multiline", "true")\
+#     .json(files, schema=SCHEMA)\
+#     .write\
+#     .mode('overwrite')\
+#     .parquet(join(WRITE_PATH, folder.split('/')[-1]))
